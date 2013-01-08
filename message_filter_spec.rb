c50fbcf2ab2311e313d32cca3949ab8b86f4c70d
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-07 21:38:16 -0800

    auto spec description

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -6,11 +6,11 @@ describe MessageFilter do
     @filter = MessageFilter.new('foo')
   end
 
-  it 'should detect message with NG word' do
+  it do
     @filter.should be_detect 'hello from foo'
   end
 
-  it 'should not detect message without NG word' do
+  it do
     @filter.should_not be_detect 'hello world!'
   end
 end
