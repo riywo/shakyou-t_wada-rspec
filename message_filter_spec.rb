f1ce2a89484ad2ed28c18d77e8a3cb289f86100c
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-07 21:27:20 -0800

    first test

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,5 +2,8 @@ require 'rspec'
 require_relative 'message_filter'
 
 describe MessageFilter do
-
+  it 'should detect message with NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello from foo').should == true
+  end
 end
