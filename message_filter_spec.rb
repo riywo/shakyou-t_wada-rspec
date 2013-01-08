94c04758af75fd1bebff9030c83bc1aa9c020f52
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-07 21:41:34 -0800

    remove \n

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,15 +2,7 @@ require 'rspec'
 require_relative 'message_filter'
 
 describe MessageFilter, 'with argument "foo"' do
-  subject do
-    MessageFilter.new('foo')
-  end
-
-  it do
-    should be_detect 'hello from foo'
-  end
-
-  it do
-    should_not be_detect 'hello world!'
-  end
+  subject { MessageFilter.new('foo') }
+  it { should be_detect 'hello from foo' }
+  it { should_not be_detect 'hello world!' }
 end
