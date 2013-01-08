b42535d949fd50b056f4e1f06c1ee5267aa76127
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-07 21:39:11 -0800

    describe additional info

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require_relative 'message_filter'
 
-describe MessageFilter do
+describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
