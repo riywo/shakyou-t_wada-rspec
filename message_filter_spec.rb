342e8156569ff20672a1b5325f561f92041070cb
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-07 22:07:52 -0800

    predicate matcher

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -11,7 +11,7 @@ describe MessageFilter do
     subject { MessageFilter.new('foo') }
     it_should_behave_like 'MessageFilter with argument "foo"'
     it 'ng_words should not be empty' do
-      subject.ng_words.empty?.should == false
+      subject.ng_words.should_not be_empty
     end
   end
 
